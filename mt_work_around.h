a54716e039bcd64f28a0dc2f4b484bd24fed3b86
Dana Everett Robinson <derobins@cori02.nersc.gov>
2020-02-12 10:59:59 -0800

    Added a smaller dataset size for testing.

--- mt_work_around.h
+++ mt_work_around.h
@@ -11,12 +11,14 @@
  *
  * For smaller systems:
  * 4,294,967,296 elements @ 32 bits per element = 17,179,869,184 bytes (16 GiB)
+ * 1,073,741,824 elements @ 32 bits per element = 4,294,967,296 bytes (4 GiB)
  */
 
 /* 1D dataset, size in elements */
-//define DSET_SIZE        274877906944       /* 1 TiB for 32-bit datatypes */
-#define DSET_SIZE        68719476736        /* 1 TiB for 64-bit datatypes, 512 GiB for 32-bit */
+//#define DSET_SIZE        274877906944       /* 1 TiB for 32-bit datatypes */
+//#define DSET_SIZE        68719476736        /* 1 TiB for 64-bit datatypes, 512 GiB for 32-bit */
 //#define DSET_SIZE        4294967296         /* 16 GiB file for smaller systems */
+#define DSET_SIZE        1073741824         /* 4 GiB file for testing */
 
 /* Chunk size, in elements (set low to force a lot of thread activity) */
 #define CHUNK_SIZE  1048576
