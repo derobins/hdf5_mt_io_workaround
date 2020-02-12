7d359a7bf6dd1787ae3009f88dae37d579e09e99
Dana Robinson <derobins@hdfgroup.org>
2020-02-12 13:26:35 -0800

    Batch run script appends to a file.

--- batch_timings.sh
+++ batch_timings.sh
@@ -4,7 +4,7 @@ for n_threads in 1 2 4 8 16 32 64
 do
     for i in {1..5}
     do
-        ./reader -a posixmt -n $n_threads data.h5
+        ./reader -a posixmt -n $n_threads data.h5 2>&1 | tee -a bt.out
     done
 done
 
